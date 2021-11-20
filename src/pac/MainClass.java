30d7ed13f7d91d63df20465c13480ea9fe738531
Dima Prudius <dimaprudius1995@gmail.com>
2021-11-20 18:10:52 +0200

    third commit

--- src/pac/MainClass.java
+++ src/pac/MainClass.java
@@ -4,6 +4,7 @@ public class MainClass {
     public static void main(String[] args) {
         System.out.println("First commit");
         System.out.println("Second commit");
+        System.out.println("Third commit");
     }
 
 
