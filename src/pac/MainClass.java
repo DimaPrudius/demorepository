fb21b44a06bd44559c159d7de263f038d6e20cad
Dima Prudius <dimaprudius1995@gmail.com>
2021-11-20 17:46:55 +0200

    second commit

--- src/pac/MainClass.java
+++ src/pac/MainClass.java
@@ -3,6 +3,7 @@ package pac;
 public class MainClass {
     public static void main(String[] args) {
         System.out.println("First commit");
+        System.out.println("Second commit");
     }
 
 
