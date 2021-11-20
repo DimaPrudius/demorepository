160be0a480789c035c3cd0793090f391809b24b7
Dima Prudius <dimaprudius1995@gmail.com>
2021-11-20 17:45:05 +0200

    delet second commit

--- src/pac/MainClass.java
+++ src/pac/MainClass.java
@@ -3,7 +3,6 @@ package pac;
 public class MainClass {
     public static void main(String[] args) {
         System.out.println("First commit");
-        System.out.println("Second commit");
     }
 
 
